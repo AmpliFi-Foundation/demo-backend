06e59c8feab8d3f36516414ba57baecf20e60056
Jeff Huang <jeffishjeff@gmail.com>
2023-02-26 08:39:28 -0700

    mint and burn positions

--- src/Bookkeeper.sol
+++ src/Bookkeeper.sol
@@ -9,7 +9,19 @@ contract Bookkeeper is ERC721 {
     Registra private immutable REGISTRA;
     address private pud;
     address private treasurer;
+
+    uint256 private lastPositionId;
     mapping(uint256 => Position) private positions;
+
+    modifier ownerOrOperatorOnly(address _owner) {
+        require(msg.sender == _owner || isApprovedForAll(_owner, msg.sender), "Bookkeeper: owner or operator only");
+        _;
+    }
+
+    modifier existingPositionOnly(uint256 _positionId) {
+        require(_exists(_positionId), "Bookkeeper: require existing _position");
+        _;
+    }
 
     constructor(string memory _name, string memory _symbol, address _registra) ERC721(_name, _symbol) {
         REGISTRA = Registra(_registra);
@@ -19,6 +31,24 @@ contract Bookkeeper is ERC721 {
     function initialize() external {
         pud = REGISTRA.pud();
         treasurer = REGISTRA.treasurer();
+    }
+
+    function mint(address _recipient) external ownerOrOperatorOnly(_recipient) returns (uint256 _positionId) {
+        _positionId = ++lastPositionId;
+        _safeMint(_recipient, _positionId);
+    }
+
+    function burn(uint256 _positionId)
+        external
+        existingPositionOnly(_positionId)
+        ownerOrOperatorOnly(ownerOf(_positionId))
+    {
+        Position storage _position = positions[_positionId];
+        require(_position.erc20Tokens.length == 0 && _position.erc721Tokens.length == 0, "Bookkeeper: not asset free");
+        require(_position.realDebt == 0 && _position.nominalDebt == 0, "Bookkeeper: not debt free");
+
+        delete positions[_positionId];
+        _burn(_positionId);
     }
 
     function onERC721Received(address, address, uint256, bytes calldata) external pure returns (bytes4) {
