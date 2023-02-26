32f6493149c61f526c16e68024ee3d4a1235a2fd
Jeff Huang <jeffishjeff@gmail.com>
2023-02-26 02:21:04 -0700

    token info, interest & penalty rates in registra

--- src/Registra.sol
+++ src/Registra.sol
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity >=0.8.19;
 
-import {Stewardable} from "./libraries/Stewardable.sol";
+import {TokenInfo} from "./structs/TokenInfo.sol";
+import {Stewardable} from "./utils/Stewardable.sol";
 
 contract Registra is Stewardable {
     address public bookkeeper;
     address public pud;
     address public treasurer;
+    uint256 public interestRateDx18;
+    uint256 public penaltyRateDx18;
+
+    mapping(address => TokenInfo) private tokenInfos;
 
     modifier zeroAddressOnly(address _address) {
         require(_address == address(0), "Registra: zero address only");
@@ -25,5 +30,21 @@ contract Registra is Stewardable {
 
     function registerTreasurer(address _treasurer) external zeroAddressOnly(treasurer) {
         treasurer = _treasurer;
+    }
+
+    function setInterestRateDx18(uint256 _interestRateDx18) external stewardOnly {
+        interestRateDx18 = _interestRateDx18;
+    }
+
+    function setPenaltyRateDx18(uint256 _penaltyRateDx18) external stewardOnly {
+        penaltyRateDx18 = _penaltyRateDx18;
+    }
+
+    function setTokenInfo(address _token, TokenInfo calldata _tokenInfo) external stewardOnly {
+        tokenInfos[_token] = _tokenInfo;
+    }
+
+    function tokenInfoOf(address _token) external view returns (TokenInfo memory _tokenInfo) {
+        _tokenInfo = tokenInfos[_token];
     }
 }
